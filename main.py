fe18bd1007aab72e11448d5a70fcad8dbf4d8c10
Iniyan <78460090+iniyan2006@users.noreply.github.com>
2022-09-03 16:37:56 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
-from config import api_id, api_hash, bot_token
 import converter
 import pyrogram
 from pyrogram import Client, filters
 
 app = Client("hand bot",
-	bot_token = bot_token,  
-	api_id = api_id,
-	api_hash = api_hash)
+	bot_token = str(os.environ.get("bot_token")),  
+	api_id = str(os.environ.get("api_id")),
+	api_hash = str(os.environ.get("api_hash"))
 @app.on_message(filters.command(["start", "help"]))
 def start_command(bot, message):
 	text  = f"""
